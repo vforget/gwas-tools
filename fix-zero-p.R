fff713568efb5f3abb4549b11cf0d48656c36fe0
Vince Forgetta <forgetta@gmail.com>
2019-11-12 14:28:05 -0500

    Minor change
    Moved a message.

--- fix-zero-p.R
+++ fix-zero-p.R
@@ -12,13 +12,13 @@ gwasf <- args[1]
 message("Loading file")
 DT <- fread(gwasf, header=TRUE)
 
-message("Computing p-values")
 # Retain original p-value
 DT[, P.old := P]
 # Set new p-value as character class, otherwise small p-values will round to 0 if numeric.
 DT[, P := NULL]
 DT[, P := as.character(P.old)]
 
+message("Computing new p-values")
 # For GWAS p-values that are equal to 0, use Rmpfr to obtain arbitraty precision using BETA and SE.
 DT[P.old==0, P := Rmpfr::format(exp(.N(pchisq((BETA/SE)^2, df=1, lower.tail=FALSE, log.p = TRUE))))]
 
